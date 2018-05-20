dfe0cc3b32a95d3ec0bdcbf805275356e6b25620
syan0127 <seyan@seas.upenn.edu>
2018-05-19 20:29:52 -0400

    update MainActivity

--- app/src/main/java/com/example/synonymreplacer/MainActivity.java
+++ app/src/main/java/com/example/synonymreplacer/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.synonymreplacer;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button start = findViewById(R.id.button_start);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void toTextActivity() {
+        Intent intent = new Intent(this, TextActivity.class);
     }
 }
